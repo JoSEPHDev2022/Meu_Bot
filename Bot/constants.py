9fb06dd8013f930c2003f7fd7ede7d7aa60ce0a6
LuizFdev <jl_ferreira16@hotmail.com>
2023-05-29 21:36:40 -0300

    Criando arquivo dedicado a constantes
    Nesse arquivo esarão presentes as constantes utilizadas na classe. Cada menu recebeu suas constantes. Também exitem constantes globais, utilizadas em diversos métodos.

---
+++ Bot/constants.py
@@ -0,0 +1,19 @@
+'''Arquivo que contém as constantes utilizadas na classe Menu do bot.
+Esse arquivo foi criado especificamente para armazenar essas constantes como
+forma de melhor visualização, manutenção e acessibilidade desses dados.'''
+
+# Constantes Menu Principal:
+MENU_ABOUT_ME = '1'
+MENU_PROJECTS = '2'
+MENU_SKILLS = '3'
+MENU_CONTACTS = '4'
+MENU_EXIT = '5'
+
+# Constantes Menu Sobre Mim:
+ABOUT_PROFESSIONAL = '1'
+ABOUT_PERSONAL = '2'
+ABOUT_RETURN_MAIN_MENU = '3'
+ABOUT_EXIT = '4'
+
+# Constantes globais:
+INVALID_ENTRY_MESSAGE = 'Opção inválida. Por favor insira um número de menu válido.'

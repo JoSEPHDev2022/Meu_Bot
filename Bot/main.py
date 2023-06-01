4d60233ea0cd98a2d1752820019a080afbb88a79
LuizFdev <jl_ferreira16@hotmail.com>
2023-06-01 17:20:36 -0300

    Adicionando Docstrings ao arquivo main

--- Bot/main.py
+++ Bot/main.py
@@ -1,8 +1,16 @@
 from bot_class import Initializer
 
 def main():
+    """
+    Função principal do programa.
+
+    Cria uma instância do inicializador do bot e executa o bot.
+
+    """
     bot = Initializer()
     bot.run()
 
+# Executa a função main apenas se o arquivo for executado diretamente,
+# não se for importado como um módulo.
 if __name__ == '__main__':
     main()
